1e0633b85610c64d5af143955f8a7c206272b3be
Korchagin Vadim Sergeevich <89642064+qerenny@users.noreply.github.com>
2025-01-26 19:49:01 +0300

    Revert "Refactor database connection handling by removing SSH tunnel dependency and simplifying connection and disconnection functions"
    This reverts commit 237db0e3741fbadafea8c3758941a0763bc3bb44.

--- database/connection.py
+++ database/connection.py
@@ -6,41 +6,60 @@ from sshtunnel import SSHTunnelForwarder
 
 logger = setup_logger('connection.database', 'database.log')
 
-
+@retry_on_error(retries=3, delay=5)
 @log_function_call(logger)
 def connect_to_db():
-    """Установка соединения с локальной базой данных"""
+    """
+    Establishes a secure connection to the database via SSH tunnel.
+    """
+    tunnel = None
     conn = None
     try:
-        # Создание подключения к базе данных
+        tunnel = SSHTunnelForwarder(
+            (API_IP, API_PORT_SSH),
+            ssh_username=API_USERNAME_SSH,
+            ssh_private_key=API_PRIVATE_KEY_SSH,
+            ssh_private_key_password=API_PASSWORD_SSH,
+            remote_bind_address=('localhost', DB_PORT),
+            local_bind_address=('localhost', 6349),
+        )
+        tunnel.start()
+        logger.info("SSH tunnel established successfully.")
+
         conn = psycopg2.connect(
             database=DB_NAME,
             user=DB_USERNAME,
             password=DB_PASSWORD,
-            host='localhost',  # или '127.0.0.1'
-            port=DB_PORT
+            host=tunnel.local_bind_host,
+            port=tunnel.local_bind_port,
         )
         conn.autocommit = True
 
         cur = conn.cursor()
-        logger.info("Database connection established successfully")
-
-        return conn, cur
+        logger.info("Database connection established successfully.")
+        return tunnel, conn, cur
 
     except Exception as e:
         logger.error(f"Error while connecting to database: {str(e)}")
+        if tunnel:
+            tunnel.close()
         if conn:
             conn.close()
         raise
 
 
 @log_function_call(logger)
-def disconnect_from_db(conn):
-    """Безопасное закрытие соединения"""
+def disconnect_from_db(conn, tunnel):
+    """
+    Closes database and SSH tunnel connections safely.
+    """
+    logger.info("Disconnecting from the database and stopping SSH tunnel.")
     try:
         if conn:
             conn.close()
-        logger.info("Database connection closed successfully")
+        if tunnel:
+            tunnel.stop()
+        logger.info("Database connections closed successfully.")
     except Exception as e:
         logger.error(f"Error while disconnecting from database: {str(e)}")
         raise
